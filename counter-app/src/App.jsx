c081a5b48b2f8c217e78b153e80c0fb102e2c03e
Esshaan Bhardwaj <ebksahal@gmail.com>
2026-07-22 19:43:06 +0530

    initial commit

---
+++ counter-app/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+  const addBtn = () => {
+    setCount(count+1);
+  }
+  const remBtn = () => {
+    setCount(count-1);
+  }
+  const reset = () => {12
+    setCount(0);
+  }
+
+  return (
+    <>
+      <h2>Count Value: {count}</h2>
+      <div className="button-group">
+        <button onClick={addBtn}>ADD</button>
+        <button onClick={remBtn}>REMOVE</button>
+        <button onClick={reset}>RESET</button>
+      </div>
+    </>
+  )
+}
+
+export default App
